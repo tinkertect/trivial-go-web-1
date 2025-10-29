edb46ef3e13dc0d7f53cfc1e0d34e2f39c0e7794
Jason Pernthaller <jason@Mac.lab>
2025-10-29 20:59:51 +0100

    adds main and modules

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func printTime(w http.ResponseWriter, r *http.Request) {
+	t := time.Now()
+
+	fmt.Fprintf(w, "time is %s\n", t.Format("2006-01-02 15:04:05"))
+}
+
+func main() {
+	http.HandleFunc("/", printTime)
+
+	fmt.Println("about to start listening on port 8000")
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		panic(err)
+	}
+}
